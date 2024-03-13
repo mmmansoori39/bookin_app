1712e7b5a671a5087108cc72d4abbaa047f92fe9
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2024-03-13 12:43:48 +0530

    added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,12 +33,12 @@ mongoose.connection.on("disconnected", () => {
 });
 
 const app = express();
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://booking-app-ebg6.onrender.com');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://booking-app-ebg6.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json({ limit: "10mb" }));
